4e69ea58d5739036466db0c48cfb632d6e5d9c26
PND Design <ipnddev@gmail.com>
2024-03-26 15:34:13 -0500

    about section - draft

---
+++ resources/views/about-section.blade.php
@@ -0,0 +1,22 @@
+<section class="flex">
+    <!-- Left Side -->
+    <div class="w-1/2 p-8">
+        <h2 class="text-2xl font-bold mb-4">About DASH</h2>
+        <p class="mb-4">
+            We're aiming to change the way the world works. By creating exciting tech that revolutionizes job boards. Reshapes temporary staffing. Rethinks recruitment. By challenging business as usual.
+        </p>
+        <p class="mb-8">
+            So why DASH? Everyone everywhere deserves quick and hassle-free access to the many jobs that make the world go around.
+        </p>
+        <a href="#" class="text-blue-500 font-bold inline-block mb-4">CHECK OUT OUR WORK</a>
+        <a href="#" class="text-blue-500 font-bold inline-block">CHECK OUT OUR WORK</a>
+    </div>
+
+    <!-- Right Side -->
+    <div class="relative">
+        <div class="relative aspect-w-16 aspect-h-9">
+            <img src="{{ asset('img/image.png') }}" class="absolute inset-0 w-full h-full object-cover" alt="Image">
+            <img src="{{ asset('img/about-1.jpeg') }}" class="absolute inset-0 w-[70%] h-full object-cover left-[30%]" alt="About Image">
+        </div>
+    </div>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     @include('header')
+    @include('about-section')
 @endsection
