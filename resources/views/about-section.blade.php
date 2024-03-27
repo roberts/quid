02a33b2fee97b5b410c4156fc0c05c8ac63bfe61
PND Design <ipnddev@gmail.com>
2024-03-26 21:36:39 -0500

    about section

--- resources/views/about-section.blade.php
+++ resources/views/about-section.blade.php
@@ -1,22 +1,26 @@
-<section class="flex">
+<section class="flex mb-48">
     <!-- Left Side -->
     <div class="w-1/2 p-8">
-        <h2 class="text-2xl font-bold mb-4">About DASH</h2>
-        <p class="mb-4">
+        <h2 class="text-6xl font-bold mb-8">About DASH</h2>
+        <p class="mb-4 text-lg pr-16 leading-8 w-2/3">
             We're aiming to change the way the world works. By creating exciting tech that revolutionizes job boards. Reshapes temporary staffing. Rethinks recruitment. By challenging business as usual.
         </p>
-        <p class="mb-8">
-            So why DASH? Everyone everywhere deserves quick and hassle-free access to the many jobs that make the world go around.
+        <h2 class="text-3xl font-bold mb-8 mt-16">So why DASH?</h2>
+        <p class="mb-4 text-lg pr-16 leading-8 w-1/2">
+            Everyone everywhere deserves quick and hassle-free access to the many jobs that make the world go around.
         </p>
-        <a href="#" class="text-blue-500 font-bold inline-block mb-4">CHECK OUT OUR WORK</a>
-        <a href="#" class="text-blue-500 font-bold inline-block">CHECK OUT OUR WORK</a>
+        <a href="#" class="inline-block mt-8 text-white font-bold py-4 px-4 bg-blue-500 shadow-lg transition-transform duration-300 transform hover:-translate-y-1">CHECK OUT OUR WORK</a>
     </div>
 
     <!-- Right Side -->
-    <div class="relative">
-    <div class="relative aspect-w-16 aspect-h-9">
-        <img src="{{ asset('img/image.png') }}" class="absolute inset-0 w-full h-full object-cover" alt="Image">
-        <img src="{{ asset('img/about-1.jpeg') }}" class="absolute inset-0 w-[70%] h-full object-cover left-[30%]" alt="About Image">
+    <div class="w-1/2 p-8">
+        <div class="relative h-full">
+            <!-- First Image -->
+            <img src="{{ asset('img/image.png') }}" class="absolute inset-0 w-24rem h-32rem object-cover transition-transform duration-300 transform hover:scale-105" alt="Image">
+
+            <!-- Second Image -->
+            <img src="{{ asset('img/about-1.jpeg') }}" class="absolute inset-0 w-24rem h-32rem object-cover left-1/3 transform -translate-x-1/2 top-32 transition-transform duration-300 transform hover:rotate-12" alt="About Image">
+
+        </div>
     </div>
-</div>
 </section>
