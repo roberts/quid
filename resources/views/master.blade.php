11a3f2372d381ce0a385aa9863f38f5f370e5888
Drew Roberts <github@drewroberts.com>
2024-03-26 23:50:09 -0400

    Add favicons

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'DASH')</title>
+    <title>@yield('title', 'Quidpixels')</title>
     <!-- Include any common CSS files or CDN links here -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="img/favicons/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body style="background-image: url('{{ asset('img/download.png') }}');" class="bg-lightPink">
     <!-- Main Content Area -->
