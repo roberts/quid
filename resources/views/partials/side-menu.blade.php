8ace39f501bcc258b794b5b009affc7f56bd250d
PND Design <ipnddev@gmail.com>
2024-03-27 20:59:50 -0500

    menu toggle

--- resources/views/partials/side-menu.blade.php
+++ resources/views/partials/side-menu.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed z-50 flex flex-col pointer-events-auto top-6 right-5 js-dashboard border-2 lg:border-dark hover:border-primary">
+<div class="fixed z-50 flex flex-col pointer-events-auto top-6 right-5 js-dashboard border-2 border-dark hover:border-primary">
     <span class="items-center justify-center w-10 h-10 cursor-pointer lg:hidden hover:border-dark" id="toggle_burger">
         <i class="w-2.5 h-1 mr-4 transform pointer-events-none">
             <svg viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -9,7 +9,7 @@
 
     <!-- complete large screen menu -->
     <div id="large_screen_menu">
-        <div class="flex items-stretch justify-between text-xs font-black uppercase font-display lg:bg-dark hover:bg-primary md:text-dark lg:text-white false" id="dashboardButton">
+        <div class="flex items-stretch justify-between text-xs font-black uppercase font-display bg-dark hover:bg-primary text-white false" id="dashboardButton">
             <span class="flex w-full h-10 px-4 items-center select-none cursor-[grab] touch-none lg:h-10 hidden lg:flex" id="dashboardText">Dashboard<span class="pl-4 inline-block" id="dash_icon">-</span></span>
         </div>
         <div class="flex flex-col p-6 border-2 border-t-0 border-solid md:pt-5 md:pl-6 md:pr-5 hidden lg:block" id="menuContainer">
@@ -56,6 +56,7 @@
     const dashboardButton = document.getElementById('dashboardButton');
     const menuContainer = document.getElementById('menuContainer');
     const dashIcon = document.getElementById('dash_icon');
+    const toggleBurger = document.getElementById('toggle_burger');
 
     dashboardButton.addEventListener('click', () => {
     
@@ -63,5 +64,28 @@
     
         const lgBlockPresent = menuContainer.classList.contains('lg:block');
         dashIcon.textContent = lgBlockPresent ? '-' : '+';
+
+        //toggle hamburger icon
+        if(toggleBurger.classList.contains('sm:hidden')){
+            toggleBurger.classList.toggle('sm:hidden')
+            largeScreenMenu.classList.toggle('hidden');
+        }
+
+    });
+
+
+    //mobile menu
+    const largeScreenMenu = document.getElementById('large_screen_menu');
+    const dashboardText = document.getElementById('dashboardText')
+
+    toggleBurger.addEventListener('click', () => {
+        toggleBurger.classList.toggle('sm:hidden')
+        largeScreenMenu.classList.remove('hidden');
+
+        //remove teh hidden class
+        //because else it wont show on mobile
+        //hidden class was added to hide it on mobile by default
+        menuContainer.classList.toggle('hidden');
+        dashboardText.classList.toggle('hidden');
     });
 </script>
