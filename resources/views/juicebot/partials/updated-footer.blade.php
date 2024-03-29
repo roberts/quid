082bb64de20610c7a6a13672641a017937f57142
PND Design <ipnddev@gmail.com>
2024-03-29 11:29:53 -0500

    updated footer

---
+++ resources/views/juicebot/partials/updated-footer.blade.php
@@ -0,0 +1,13 @@
+<section class="flex p-8 flex-col items-center md:items-start gap-y-">
+
+    <!-- Central image -->
+    <div class="relative mx-auto">
+         <!-- Twitter logo positioned near top left of the central image -->
+    <img src="{{asset('img/QUIDWEB/Xtwitter.png')}}" class="absolute left-4 top-4 h-12 w-24" alt="Twitter Logo">
+
+        <img src="{{asset('img/QUIDWEB/SQUID_WHITE.png')}}" class="h-64 w-64" alt="Central Image">
+        
+        <!-- Telegram logo positioned near top right of the central image -->
+        <img src="{{asset('img/QUIDWEB/telegram.png')}}" class="absolute right-4 top-4 h-12 w-24" alt="Telegram Logo">
+    </div>
+</section>
