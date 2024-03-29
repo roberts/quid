82162984836c25630d49d324f97fba575b905045
Drew Roberts <github@drewroberts.com>
2024-03-29 07:01:53 -0400

    Add quidpixels links

--- resources/views/juicebot/partials/footer.blade.php
+++ resources/views/juicebot/partials/footer.blade.php
@@ -4,25 +4,25 @@
         class="w-[40vw] md:w-[15vw] md:-ml-6 -mb-5" style="color:transparent" 
         src="{{asset('img/juicebot/juicebox.webp')}}">
         <div class="hidden md:flex flex-col">
-            <a target="_blank" href="/whitepaper">
+            <a target="_blank" href="/litepaper">
                 <span class="text-white hover:text-juiceGreen">Litepaper</span>
             </a>
             <a target="_blank" href="/disclaimer">
                 <span class="text-white hover:text-juiceGreen">Disclaimer</span>
             </a>
-            <a target="_blank" href="mailto:info@juicebot.app">
-                <span class="text-white hover:text-juiceGreen">info@juicebot.app</span>
+            <a target="_blank" href="mailto:retards@quidpixels.com">
+                <span class="text-white hover:text-juiceGreen">retards@quidpixels.com</span>
             </a>
         </div>
     </div>
     <div class="flex flex-col md:items-end gap-y-5">
         <div class="flex items-center gap-x-3">
-            <a href="https://t.me/JuiceBotApp" target="_blank" rel="nofollow">
+            <a href="https://t.me/Quidpixels" target="_blank" rel="nofollow">
             <img alt="icon" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" class="w-[13vw] h-[13vw] md:w-[3vw] md:h-[3vw]" 
             style="color:transparent"
             src="{{asset('img/juicebot/telegram.webp')}}">
         </a>
-        <a href="https://twitter.com/juicebotapp" target="_blank" rel="nofollow">
+        <a href="https://x.com/Quidpixels" target="_blank" rel="nofollow">
             <img alt="icon" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" class="w-[13vw] h-[13vw] md:w-[3vw] md:h-[3vw]"
             src="{{asset('img/juicebot/twitter.webp')}}">
         </a>
@@ -30,7 +30,7 @@
             <img alt="icon" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" class="w-[13vw] h-[13vw] md:w-[3vw] md:h-[3vw]" style="color:transparent" 
             src="{{asset('img/juicebot/coinmarketcap.webp')}}">
         </a>
-        <a href="https://www.dextools.io/app/en/ether/pair-explorer/0xba8381ed6122829daa46b0038d980d1c6e17cd7c?t=1709787493219" target="_blank" rel="nofollow">
+        <a href="" target="_blank" rel="nofollow">
             <img alt="icon" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" class="w-[13vw] h-[13vw] md:w-[3vw] md:h-[3vw]" style="color:transparent" 
             src="{{asset('img/juicebot/dex.webp')}}">
         </a>
@@ -40,10 +40,10 @@
         </a>
     </div>
         <div class="flex flex-col items-center md:hidden">
-            <a href="/whitepaper">
+            <a href="/litepaper">
                 <span class="text-white hover:text-juiceGreen">Litepaper</span>
             </a>
-            <a href="/disclaimer">
+            <a href="/none">
                 <span class="text-white hover:text-juiceGreen">Disclaimer</span>
             </a>
             <a href="mailto:info@juicebot.app">
