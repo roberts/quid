3e178fab95ff93d5128a0613508a03831f3086e1
Drew Roberts <github@drewroberts.com>
2024-03-31 08:37:35 -0400

    Comment out Liquidity Pool breakdown

--- resources/views/juicebot/partials/tokenomics.blade.php
+++ resources/views/juicebot/partials/tokenomics.blade.php
@@ -1,14 +1,13 @@
 <section class="flex p-8 md:px-14 flex-col md:flex-col items-center md:pl-[6rem] gap-y-6">
     <div class="flex flex-col justify-center items-center text-center md:items-center gap-y-6 md:w-1/2">
         <h3 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white">Tokenomics</h3>
-        <p class="text-white text-base text-center leading-relaxed">100% on the Liquidity Pool</p>
-    
+        <p class="text-white text-base text-center leading-relaxed">100% in the Liquidity Pool</p>
     </div>
     <div class="flex flex-col justify-center items-center mx-auto w-full max-h-[285px] md:max-h-[600px] h-min bg-primary relative">
         <div class="scale-[30%] sm:scale-[40%] md:scale-[70%] lg:scale-[85%] xl:scale-[90%] origin-center md:translate-x-0 w-[1100px] h-[659px] top-0 left-0">
             <div class="relative w-[100%] h-[659px] -mt-[4rem]">
-                // <img class="absolute w-[579px] h-[578px] top-[10px] left-[265px] object-cover" alt="Tokenomics" src="{{asset('img/QUIDWEB/TOKENOMICS.png')}}">
-
+                <img class="absolute w-[579px] h-[578px] top-[10px] left-[265px] object-cover" alt="Tokenomics" src="{{asset('img/QUIDWEB/TOKENOMICS.png')}}">
+<!-- 
                 <p class="absolute top-[69px] left-[897px] font-normal text-transparent text-[48px] tracking-[0] leading-[normal]">
                     <span class="text-[#00d000]">0%</span><span class="text-white">&nbsp;</span>
                     <span class="text-white text-[60px]"> </span>
@@ -19,7 +18,7 @@
                 <span class="text-white text-[26px]"> MARKETING</span></p>
                 <p class="absolute top-[500px] left-[101px] font-normal text-transparent text-[48px] text-right tracking-[0] leading-[normal]">
                     <span class="text-[#00d000]">0%</span><span class="text-white text-[60px]"> </span>
-                    <span class="text-white text-[26px]"> <!-- -->FARMING <br>REWARDS</span>
+                    <span class="text-white text-[26px]"> FARMING <br>REWARDS</span>
                 </p>
                 <p class="top-[296px] left-[976px] absolute font-normal text-transparent text-[48px] tracking-[0] leading-[normal]">
                     <span class="text-[#00d000]">0%</span><span class="text-white">&nbsp;</span>
@@ -46,6 +45,7 @@
                 <img class="absolute w-[324px] h-[78px] top-[380px] left-[752px]" alt="Vector" src="https://c.animaapp.com/vuWIzqEJ/img/vector-6.svg">
                 <img class="absolute w-[331px] h-[2px] top-[578px] left-[643px]" alt="Vector" src="https://c.animaapp.com/vuWIzqEJ/img/vector-7.svg">
                 <img class="absolute w-[579px] h-[578px] top-[50px] left-[265px] object-cover" alt="Tokenomics" src="{{asset('img/QUIDWEB/TOKENOMICS.png')}}">
+                -->
             </div>
         </div>
     </div>
